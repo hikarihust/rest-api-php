6e8fe6f5a844629c3fad278fd4d2f0e7f13aafb6
vudinhquang <vudinhquangk53@gmail.com>
2022-08-16 14:38:05 +0900

    Create a table to store resource data

---
+++ 4.create-a-restful-api/create_task_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE task (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    priority INT DEFAULT NULL,
+    is_completed BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id),
+    INDEX (name)
+);
