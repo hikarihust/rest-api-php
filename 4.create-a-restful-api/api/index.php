d949d4327b1f78915d372c627aba377b959e4fe3
vudinhquang <vudinhquangk53@gmail.com>
2022-08-16 15:13:05 +0900

    Connect to the database from PHP: add a Database class

--- 4.create-a-restful-api/api/index.php
+++ 4.create-a-restful-api/api/index.php
@@ -24,6 +24,10 @@ if ($resource != "tasks") {
 
 header("Content-type: application/json; charset=UTF-8");
 
+$database = new Database("localhost", "api_db", "root", "");
+
+$database->getConnection();
+
 $controller = new TaskController;
 
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
---
+++ 4.create-a-restful-api/src/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database
+{
+    private $host;
+    private $name;
+    private $user;
+    private $password;
+
+    public function __construct(
+        string $host,
+        string $name,
+        string $user,
+        string $password
+    ) {
+        $this->host = $host;
+        $this->name = $name;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public function getConnection(): PDO
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+
+        return new PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
+}
