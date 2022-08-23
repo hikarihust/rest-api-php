871e82939d52fcf362df81ef4c94f47a80f6cd25
vudinhquang <vudinhquangk53@gmail.com>
2022-08-23 22:54:49 +0900

    Validate the access token and decode its contents

--- 4.create-a-restful-api/api/index.php
+++ 4.create-a-restful-api/api/index.php
@@ -24,15 +24,14 @@ $database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $
 
 $user_gateway = new UserGateway($database);
 
-echo json_encode($_SERVER["HTTP_AUTHORIZATION"]);
-
-exit;
-
 $auth = new Auth($user_gateway);
 
-if (!$auth->authenticateAPIKey()) {
+if (!$auth->authenticateAccessToken()) {
     exit;
 }
+
+echo json_encode("valid authentication");
+exit;
 
 $user_id = $auth->getUserID();
 
