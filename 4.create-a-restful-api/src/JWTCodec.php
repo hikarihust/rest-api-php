2f26564e59730861338dbb831a2747f099cd35e3
vudinhquang <vudinhquangk53@gmail.com>
2022-08-26 21:45:30 +0900

    Add a method to decode the payload from the JWT

--- 4.create-a-restful-api/src/JWTCodec.php
+++ 4.create-a-restful-api/src/JWTCodec.php
@@ -24,12 +24,53 @@ class JWTCodec
         return $header . "." . $payload . "." . $signature;
     }
 
+    public function decode(string $token): array
+    {
+        if (preg_match(
+            "/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/",
+            $token,
+            $matches
+        ) !== 1) {
+
+            throw new InvalidArgumentException("invalid token format");
+        }
+
+        $signature = hash_hmac(
+            "sha256",
+            $matches["header"] . "." . $matches["payload"],
+            "3979244226452948404D635166546A576E5A7234743777217A25432A462D4A61",
+            true
+        );
+
+        $signature_from_token = $this->base64urlDecode($matches["signature"]);
+
+        if (!hash_equals($signature, $signature_from_token)) {
+
+            throw new Exception("signature doesn't match");
+        }
+
+        $payload = json_decode($this->base64urlDecode($matches["payload"]), true);
+
+        return $payload;
+    }
+
     private function base64urlEncode(string $text): string
     {
         return str_replace(
             ["+", "/", "="],
             ["-", "_", ""],
             base64_encode($text)
+        );
+    }
+
+    private function base64urlDecode(string $text): string
+    {
+        return base64_decode(
+            str_replace(
+                ["-", "_"],
+                ["+", "/"],
+                $text
+            )
         );
     }
 }
